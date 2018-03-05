42703284862333168751f5323184dc3330ac1968
fatesq <fatekiki@gmail.com>
2018-03-05 18:05:33 +0800

    work

--- src/routes/Map/SearchBox.js
+++ src/routes/Map/SearchBox.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { findDOMNode } from 'react-dom';
+import { SearchBar, ActionSheet, Icon } from 'antd-mobile';
 import styles from './index.less';
 
 export default class SearchBox extends React.PureComponent {
-  map = this.props.__map__;
+  constructor(props) {
+    super(props);
+    this.map = this.props.__map__; // 接收地图实例
+  }
   componentDidMount() {
-    window.AMap.service(['AMap.PlaceSearch', 'AMap.Autocomplete'], () => {
+    window.AMap.service(['AMap.PlaceSearch', 'AMap.Autocomplete', 'AMap.Geolocation'], () => {
       try {
-        this.ready();
+        this.ready(); // 初始化地图插件
       } catch (e) {
         console.error(e);
       }
@@ -15,25 +20,25 @@ export default class SearchBox extends React.PureComponent {
 
   ready = () => {
     const { AMap } = window;
-    const auto = new AMap.Autocomplete({ input: 'tipinput' });
-    const placeSearch = new AMap.PlaceSearch({
-      pageSize: 5,
-      pageIndex: 1,
-      map: this.map,
-      panel: 'poiList',
-    });
-    AMap.event.addListener(auto, 'select', (e) => {
-      console.log(e);
+    const auto = new AMap.Autocomplete({ input: 'tipinput' }); // 挂查询补全
+    const geolocation = new window.AMap.Geolocation(); // 挂定位
+    const placeSearch = new AMap.PlaceSearch({ pageSize: 5, pageIndex: 1, map: this.map, panel: 'poiList' }); // 挂搜索
+    this.map.addControl(geolocation);
+    geolocation.getCurrentPosition();
+    AMap.event.addListener(auto, 'select', (e) => { // 监听选择项
       // 设置搜索的城市
       placeSearch.setCity(e.poi.adcode);
       // 开始搜索对应的poi名称
       placeSearch.search(e.poi.name, (status, results) => {
         console.log(status, results);
+        // this.showActionSheet(results);
         if (results.pois && results.pois.length > 0) {
           console.log(results);
         }
       });
     });
+    AMap.event.addListener(geolocation, 'complete', (e) => { console.log(e); }); // 返回定位信息
+    AMap.event.addListener(geolocation, 'error', (e) => { console.log(e); }); // 返回定位出错信息
   }
 
   render() {
@@ -41,14 +46,16 @@ export default class SearchBox extends React.PureComponent {
     return (
       <div>
         <div className={styles.searchBox}>
-          <input id="tipinput" className={styles.tipinput} type="input" placeholder="请输入关键字搜索" />
-          <div>
-            <div>&#10005;</div>
-          </div>
+          <SearchBar ref={(e) => {
+                try { e.inputRef.id = 'tipinput'; } catch (error) { console.log(''); }
+              }
+            }
+          />
         </div>
         <div id="panel" className={styles.panel}>
+          <a className={styles.showHideBtn}>&nbsp;</a>
           <div id="emptyTip">没有内容！</div>
-          <div id="poiList" />
+          <div id="poiList" className={styles.poiList} />
         </div>
       </div>
     );
--- src/routes/Map/index.js
+++ src/routes/Map/index.js
@@ -7,16 +7,13 @@ export default class MapC extends React.PureComponent {
   constructor(props) {
     super(props);
     this.mapEvents = {
-      created(map) {
-        console.log(map);
-        console.log(AMap);
-      },
+      created(map) {},
     };
   }
   componentDidMount() {}
   render() {
     return (
-      <div id="123" style={{ width: '100%', height: '100%' }}>
+      <div style={{ width: '100%', height: '100%' }}>
         <Map amapkey="788e08def03f95c670944fe2c78fa76f" events={this.mapEvents}>
           <SearchBox />
         </Map>
